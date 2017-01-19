710a3356983b94ca413b94396951a3a13ef58937
sudheer694@gmail.com <sudheer694@gmail.com>
2017-01-19 06:24:54 +0530

    Pushing the code to Master Branch

---
+++ src/MainFlow.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class MainFlow {
+	static WebDriver driver ;
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+		driver= new FirefoxDriver();
+		driver.get("http://www.retailmenot.com");
+		driver.findElement(By.cssSelector("span.button-close.js-close")).click();
+		driver.findElement(By.linkText("Coupons")).click();
+		driver.findElement(By.linkText("Product Deals")).click();
+		driver.findElement(By.xpath("(//a[contains(text(),'Electronics')])[2]")).click();		
+	}
+
+}
